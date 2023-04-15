dea50a7a6762a264e89248000017b98ff7a0940e
arijit <arijitmaiti@technoexponent.com>
2023-04-16 01:45:22 +0530

    2023-04-16T01:45:22+05:30

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ const calculateDate = async (
 };
 const directory = process.cwd();
 
-calculateDate(4, "months", 20)
+calculateDate(4, "months", 10)
   .then((isSuccess) => {
     console.log(isSuccess);
     if (isSuccess) {
